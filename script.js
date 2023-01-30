5bebf9bc2925eb71a1337a00626c4051b530fdfa
flopie <dehaan@dataprovider.com>
2023-01-30 16:32:17 +0100

    use trading card splash screen

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
-const div = document.querySelector(".typewriter-text");
-const caret = document.querySelector(".caret");
-const jobTitleElement = document.querySelector(".job-title");
 const yearsExperience = document.querySelector(".years-experience");
-const jobTitle = "frontend enginer".split("");
-const correction = "er".split("");
-const DELAY_IN_MS = 100;
-
-let i = 0;
-let innerI = 0;
-
-setTimeout(() => {
-  caret.style.color = "white";
-  setTimeout(() => {
-    const intervalId = setInterval(() => {
-      if (i === jobTitle.length) {
-        clearInterval(intervalId);
-        setTimeout(() => {
-          div.textContent = div.textContent.slice(0, -1);
-          const innerIntervalId = setInterval(() => {
-            if (innerI === correction.length) {
-              clearInterval(innerIntervalId);
-              setTimeout(() => {
-                caret.style.color = "transparent";
-                jobTitleElement.style.marginLeft = 0;
-                jobTitleElement.style.justifyContent = "center";
-                setTimeout(() => {
-                  caret.remove();
-                }, DELAY_IN_MS * 4);
-              }, DELAY_IN_MS * 10);
-            } else {
-              div.innerHTML += correction[innerI];
-              innerI += 1;
-            }
-          }, DELAY_IN_MS * 2);
-        }, DELAY_IN_MS * 10);
-      } else {
-        div.innerHTML += jobTitle[i];
-        i += 1;
-      }
-    }, DELAY_IN_MS);
-  }, DELAY_IN_MS * 10);
-}, DELAY_IN_MS * 4);
 
 yearsExperience.innerHTML = new Date().getFullYear() - 2017;
 
@@ -59,3 +17,50 @@ function validateContactForm() {
   submitButton.title = isContactFormValid ? "" : "Please fill in all fields.";
 }
 
+const tradingCard = document.querySelector('.trading-card');
+
+let bounds;
+
+function rotateToMouse(e) {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  const leftX = mouseX - bounds.x;
+  const topY = mouseY - bounds.y;
+  const center = {
+    x: leftX - bounds.width / 2,
+    y: topY - bounds.height / 2
+  }
+  const distance = Math.sqrt(center.x**2 + center.y**2);
+  
+  tradingCard.style.transform = `
+    scale3d(1.07, 1.07, 1.07)
+    rotate3d(
+      ${center.y / 100},
+      ${-center.x / 100},
+      0,
+      ${Math.log(distance)* 2}deg
+    )
+  `;
+  
+  tradingCard.querySelector('.glow').style.backgroundImage = `
+    radial-gradient(
+      circle at
+      ${center.x * 2 + bounds.width/2}px
+      ${center.y * 2 + bounds.height/2}px,
+      #ffffff55,
+      transparent
+    )
+  `;
+}
+
+tradingCard.addEventListener('mouseenter', () => {
+  bounds = tradingCard.getBoundingClientRect();
+  document.addEventListener('mousemove', rotateToMouse);
+});
+
+tradingCard.addEventListener('mouseleave', () => {
+  document.removeEventListener('mousemove', rotateToMouse);
+  tradingCard.style.transform = '';
+  tradingCard.style.background = '';
+  tradingCard.querySelector('.glow').style.backgroundImage = '';
+});
